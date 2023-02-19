36238540e4d239d84d4b052bfb042eefb5e21db8
barrettMCW <mjbarrett@mcw.edu>
2023-02-19 09:58:28 -0600

    fix grafana endpoint

--- pipe/grafana.sh
+++ pipe/grafana.sh
@@ -12,12 +12,7 @@ do
     arr+=(${line%\ *})
 done <<< "$(redis-cli lrange queue 0 -1)"
 queue=$(jq -rcn '$ARGS.positional' --args -- "${arr[@]}")
-# json="$(redis-cli lrange queue 0 -1 2> /dev/null)"
-# echo $json
-# queue="$(echo $json | jq -rcn '')"
-# [[ $queue == "null" ]] && queue="[]"
-# echo $processing
-echo $queue
+
 # form complete json data
 JSON_STRING=$(jq -n \
                   --argjson pipeline_static_configs "$static" \
