2d85d2eda74b70019bdad835410fbad54387cfac
barrettMCW <mjbarrett@mcw.edu>
2023-02-21 00:18:22 -0600

    fix queuing duplicate while processing

--- pipe/queueImage.sh
+++ pipe/queueImage.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # adds image to redis queue
-/docker/log.sh INFO "Added to Queue: $1"
 
-# if input is already queued, ignore it. 
-[[ ! -z $(redis-cli lrange queue 0 -1 | grep \'$1\'; exit 0) ]] && continue
-
+# if input is already queued or running, ignore it. 
+[[ ! -z $(redis-cli lrange queue 0 -1 | grep ${1##*/}; exit 0) ]] && 
+    /docker/log.sh WARN "Image with same name is already running! Ignoring: $1" && 
+    exit 1
+[[ ! -z $(redis-cli lrange running 0 -1 | grep ${1##*/}; exit 0) ]] && 
+    /docker/log.sh WARN "Image with same name is already queued! Ignoring: $1" && 
+    exit 1
+    
 # if priority import, prepend, else, append
+/docker/log.sh INFO "Added to Queue: $1"
 [[ "${a##*/}" =~ "PRIORITY_" ]] &&
     redis-cli lpush queue "$1 $2" 1> /dev/null 2>& 1 ||
     redis-cli rpush queue "$1 $2" 1> /dev/null 2>& 1
