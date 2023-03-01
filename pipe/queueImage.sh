dedb8ff9ccc68b75af4be82a1ceef30393bca19a
barrettMCW <103449618+barrettMCW@users.noreply.github.com>
2023-03-01 10:58:52 -0600

    only ignore exact matches for queue

--- pipe/queueImage.sh
+++ pipe/queueImage.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 # adds image to redis queue
 main(){
-    # if input is already queued or running, ignore it. 
-    [[ ! -z $(redis-cli lrange queue 0 -1 | grep ${1##*/}; exit 0) ]] && 
-        /docker/log.sh WARN "Image with same name is already running! Ignoring: $1" && 
-        return
-    [[ ! -z $(redis-cli lrange running 0 -1 | grep ${1##*/}; exit 0) ]] && 
-        /docker/log.sh WARN "Image with same name is already queued! Ignoring: $1" && 
+    # if input is already queued, ignore it. 
+    [[ ! -z $(redis-cli lrange queue 0 -1 | grep ${1}; exit 0) ]] && 
+        /docker/log.sh WARN "This WSI is already queued! Ignoring: $1" && 
         return
         
     # if priority import, prepend, else, append
     /docker/log.sh INFO "Added to Queue: $1"
-    [[ "${a##*/}" =~ "PRIORITY_" ]] &&
+    [[ "${1##*/}" =~ "PRIORITY_" ]] &&
         redis-cli lpush queue "$1 $2" 1> /dev/null 2>& 1 ||
         redis-cli rpush queue "$1 $2" 1> /dev/null 2>& 1
 }
